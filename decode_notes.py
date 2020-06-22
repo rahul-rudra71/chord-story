942c17a0f4ba138facc65f064e376521db840cb1
michaelarobertsjr <34015906+michaelarobertsjr@users.noreply.github.com>
2020-06-21 21:35:34 -0400

    Update decode_notes.py
    Expanded on string assignment, added previous note tracking

--- decode_notes.py
+++ decode_notes.py
@@ -1,5 +1,6 @@
 import librosa
 import numpy as np
+import random
 
 def detect_pitch(magnitudes, pitches, t):
   index = magnitudes[:, t].argmax()
@@ -23,6 +24,70 @@ def trim_onsets(onsets, times):
 
     return onsets, times
 
+def trim_times(times):
+    # Trim onset times to 2 decimal places
+    for index, time in enumerate(times):
+        times[index] = round(time, 2)
+
+    return times
+
+def assign_string(nt, prev):
+    # string 0 = E4 - B5
+    # string 1 = B3 - F#5
+    # string 2 = G3 - D5
+    # string 3 = D3 - A4
+    # string 4 = A2 - E4
+    # string 5 = E2 - B3
+    string0 = ['E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4', 'C5', 'C#5', 'D5',
+               'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5']
+    string1 = ['B3', 'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4',
+               'A#4', 'B4', 'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5']
+    string2 = ['G3', 'G#3', 'A3', 'A#3', 'B3', 'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4',
+               'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4', 'C5', 'C#5', 'D5']
+    string3 = ['D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3', 'C4',
+               'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4']
+    string4 = ['A2', 'A#2', 'B2', 'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3',
+               'G#3', 'A3', 'A#3', 'B3', 'C4', 'C#4', 'D4', 'D#4', 'E4']
+    string5 = ['E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2', 'C3', 'C#3', 'D3',
+               'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3']
+
+    out = ''
+    possible = []
+    if int(nt[len(nt) - 1]) < 2:
+        out = 'low'
+
+    if int(nt[len(nt) - 1]) > 5:
+        out = 'high'
+
+    if nt in string5:
+        possible.append('5')
+
+    if nt in string4:
+        possible.append('4')
+
+    if nt in string3:
+        possible.append('3')
+
+    if nt in string2:
+        possible.append('2')
+
+    if nt in string1:
+        possible.append('1')
+
+    if nt in string0:
+        possible.append('0')
+
+    # Check possible strings and assign note to one of them while ensuring the previous string
+    # is not reused for better playability
+    if len(possible) != 0:
+        if prev in possible and len(possible) != 1:
+            possible.remove(prev)
+            out = random.choice(possible)
+        else:
+            out = random.choice(possible)
+
+    return out
+
 def decode(input_wav_file):
     filename = input_wav_file
 
@@ -38,42 +103,34 @@ def decode(input_wav_file):
 
     # Trim onset array
     trimmed_onset, new_times = trim_onsets(onset_frames, timestamps)
+    trimmed_times = trim_times(new_times)
 
     x = 0
+    previous = ''
     keyout = {}
     while x < len(trimmed_onset):
         pitch_start = detect_pitch(magnitudes=mags, pitches=ps, t=trimmed_onset[x])
         note = librosa.core.hz_to_note(pitch_start)
-        #the string numbers correspond to the strings in
-        #Obstacle.py.  Simply replace the print statements
-        #with the same number and pass to Obstacle.py
+        # The string numbers correspond to the strings in
+        # Obstacle.py.  Iterate through the output dictionary
+        # to obtain obstacle string placements and times
 
-        #string 0 (high) = C7 - infin
-        #string 1 = C6 - B6
-        #string 2 = C5 - B5
-        #string 3 = C4 - B4
-        #string 4 = C3 - B3
-        #string 5 = A0 - B2
-        if int(note[len(note) - 1]) < 3:
-            keyout[round(new_times[x], 2)] = '5'
+        temp_assign = assign_string(note, previous)
+        if temp_assign == 'low':
+            # Note must be below range, change to 2 to be assigned
+            note = note[:-1] + '2'
+            temp_assign = assign_string(note, previous)
+        elif temp_assign == '':
+            # Note must be somewhere from C2 - D#2, change to 3 to be assigned
+            note = note[:-1] + '3'
+            temp_assign = assign_string(note, previous)
+        elif temp_assign == 'high':
+            # Note must be above range, change to 5 to be assigned
+            note = note[:-1] + '5'
+            temp_assign = assign_string(note, previous)
 
-        elif int(note[len(note) - 1]) == 3:
-            keyout[round(new_times[x], 2)] = '4'
-
-        elif int(note[len(note) - 1]) == 4:
-            keyout[round(new_times[x], 2)] = '3'
-
-        elif int(note[len(note) - 1]) == 5:
-            keyout[round(new_times[x], 2)] = '2'
-
-        elif int(note[len(note) - 1]) == 6:
-            keyout[round(new_times[x], 2)] = '1'
-
-        elif int(note[len(note) - 1]) >= 7:
-            keyout[round(new_times[x], 2)] = '0'
-
-        else:
-            print('Error.')
+        keyout[trimmed_times[x]] = temp_assign
+        previous = temp_assign
 
         x = x + 1
     # Output dictionary of times and string assignments
