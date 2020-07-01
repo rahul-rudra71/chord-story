f99582736ad18a198134c559fde4ff9485b76c95
michaelarobertsjr <34015906+michaelarobertsjr@users.noreply.github.com>
2020-07-01 02:07:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='chordstory',
+    name='chord_story',
     version='0.5.0',
     author='Chord Story',
     description='fundamental game play and audio processing',
-    packages = find_packages('chord_story'),
+    py_modules=['chord_story.main'],
+    packages = ['chord_story',],
     install_requires=['librosa','numba==0.48.0','pygame==2.0.0.dev10'],
 
     entry_points={
         'console_scripts': [
-            'chordStory = chord_story.main:start'
+            'chordstory = chord_story.main:main_menu'
         ]
     }
 )
