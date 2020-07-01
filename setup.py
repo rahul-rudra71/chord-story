681c69ced1a112ed128e618974a1d04d98a32487
michaelarobertsjr <34015906+michaelarobertsjr@users.noreply.github.com>
2020-06-30 22:28:17 -0400

    Update setup.py
    Added entry points for package

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='chordstory',
     version='0.5.0',
     author='Chord Story',
     description='fundamental game play and audio processing',
-    install_requires=['librosa','numba==0.48.0','pygame==2.0.0.dev10']
-)
+    packages = find_packages('chord_story'),
+    install_requires=['librosa','numba==0.48.0','pygame==2.0.0.dev10'],
 
+    entry_points={
+        'console_scripts': [
+            'chordStory = chord_story.main:start'
+        ]
+    }
+)
