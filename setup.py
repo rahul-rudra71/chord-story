bfe7828b5403f5026bde1b9c08bbbc82b46c751d
rahul-rudra71 <42813401+rahul-rudra71@users.noreply.github.com>
2020-06-30 22:02:55 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='chordstory',
+    version='0.5.0',
+    author='Chord Story',
+    description='fundamental game play and audio processing',
+    install_requires=['librosa','numba==0.48.0','pygame==2.0.0.dev10']
+)
+
