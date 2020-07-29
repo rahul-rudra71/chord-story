cbac9b71b71cf193e3345ebc6485b6b2d48ccfcd
isabellabond <ibond713@gmail.com>
2020-07-29 13:21:54 -0400

    Fix the button spacing

--- chord_story/main.py
+++ chord_story/main.py
@@ -112,18 +112,18 @@ def select_difficulty():
         hardH_image = pygame.image.load("assets/images/buttons/hardH.png")
 
         easyRects = [
-            easy_image.get_rect(center=(300, 120)),
-            easyH_image.get_rect(center=(300, 120)),
+            easy_image.get_rect(center=(300, 130)),
+            easyH_image.get_rect(center=(300, 130)),
         ]
 
         medRects = [
-            med_image.get_rect(center=(300, 220)),
-            medH_image.get_rect(center=(300, 220)),
+            med_image.get_rect(center=(300, 230)),
+            medH_image.get_rect(center=(300, 230)),
         ]
 
         hardRects = [
-            hard_image.get_rect(center=(300, 320)),
-            hardH_image.get_rect(center=(300, 320)),
+            hard_image.get_rect(center=(300, 330)),
+            hardH_image.get_rect(center=(300, 330)),
         ]
 
         mousex, mousey = pygame.mouse.get_pos()
@@ -149,64 +149,6 @@ def select_difficulty():
                 game.difficulty = 0.25
                 select = False
 
-        #font = pygame.font.Font("freesansbold.ttf", 45)
-        #text = font.render("CHOOSE THE DIFFICULTY", True, (0, 0, 0))
-        #text2 = font.render("CHOOSE THE DIFFICULTY", True, (255, 255, 255))
-        #text_rect = text.get_rect(center=(screen.get_width() / 2, 45))
-        #text_rect2 = text.get_rect(center=((screen.get_width() / 2) + 2, 47))
-        #screen.blit(text, text_rect)
-        #screen.blit(text2, text_rect2)
-
-
-
-        # easy_button = pygame.Rect(screen.get_width() / 2 - 72.5, 100, 145, 60)
-        # medium_button = pygame.Rect(screen.get_width() / 2 - 72.5, 200, 145, 60)
-        # hard_button = pygame.Rect(screen.get_width() / 2 - 72.5, 300, 145, 60)
-
-        # render buttons
-        # pygame.draw.rect(screen, (255, 255, 255), easy_button)
-        # pygame.draw.rect(screen, (255, 255, 255), medium_button)
-        # pygame.draw.rect(screen, (255, 255, 255), hard_button)
-
-        # highlight = (212, 221, 255)
-
-        # harder difficult = smaller interval between notes
-        # if easy_button.collidepoint((mousex, mousey)):
-        #     pygame.draw.rect(screen, highlight, easy_button)
-        #     if click:
-        #         game.difficulty = 0.5
-        #         select = False
-        # if medium_button.collidepoint((mousex, mousey)):
-        #     pygame.draw.rect(screen, highlight, medium_button)
-        #     if click:
-        #         game.difficulty = 0.35
-        #         select = False
-        # if hard_button.collidepoint((mousex, mousey)):
-        #     pygame.draw.rect(screen, highlight, hard_button)
-        #     if click:
-        #         game.difficulty = 0.25
-        #         select = False
-
-        # # easy text
-        # font = pygame.font.Font("freesansbold.ttf", 35)
-        # text = font.render("EASY", True, (0, 0, 0))
-        # text2 = font.render("EASY", True, (224, 132, 132))
-        # screen.blit(text, (247, 116))
-        # screen.blit(text2, (249, 118))
-
-        # # medium text
-        # font = pygame.font.Font("freesansbold.ttf", 30)
-        # text = font.render("MEDIUM", True, (0, 0, 0))
-        # text2 = font.render("MEDIUM", True, (224, 132, 132))
-        # screen.blit(text, (237, 216))
-        # screen.blit(text2, (239, 218))
-
-        # # hard text
-        # font = pygame.font.Font("freesansbold.ttf", 35)
-        # text = font.render("HARD", True, (0, 0, 0))
-        # text2 = font.render("HARD", True, (224, 132, 132))
-        # screen.blit(text, (247, 316))
-        # screen.blit(text2, (249, 318))
 
         for event in pygame.event.get():  # event loop
             if event.type == QUIT:
